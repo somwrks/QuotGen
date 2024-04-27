d856c49b022316c79230de9327bd0c3015e6005d
Som <somwrks@gmail.com>
2024-04-27 15:46:49 +0530

    e

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-
+import {Amplify} from "aws-amplify";
+import awsExports from "../src/aws-exports";
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
